39cda5434061b7f798ec1ffa9824aeb00165d7ab
Potapenko Egor <73658660+Shara1p@users.noreply.github.com>
2022-02-08 22:08:14 +0300

    Add files via upload

---
+++ Bob/Bob.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <string>
+
+void Bob(std::string phrase) {
+    bool upper = false;
+    bool question = false;
+    bool empty = false;
+
+    for (int i = 0; i < phrase.length(); i++) {
+        if (phrase[i] == ' ') {
+            empty = true;
+        }
+        if (!isalpha(phrase[i])) {
+            continue;
+        }
+        else if (std::isupper(phrase[i])) {
+            upper = true;
+            empty = false;
+        }
+        else {
+            upper = false;
+            empty = false;
+        }
+    }
+
+    if (phrase[phrase.length() - 1] == '?') {
+        question = true;
+    }
+    if ((upper) and (question)) {
+        std::cout << "Calm down, I know what I'm doing!" << std::endl;
+    }
+    else if (upper) {
+        std::cout << "Whoa, chill out!" << std::endl;
+    }
+    else if (question) {
+        std::cout << "Sure" << std::endl;
+    }
+    else if (empty) {
+        std::cout << "Fine. Be that way!" << std::endl;
+    }
+    else std::cout << "Whatever" << std::endl;
+}
+
+int main()
+{
+    std::string phrase;
+    getline(std::cin, phrase);
+    Bob(phrase);
+}
---
+++ Cocktail/Cocktail.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+void shaker(int* Array, int size) {
+
+	int left = 0;
+	int right = size - 1;
+	int temp;
+
+	while ((left != right - 1)) {
+		
+		for (int i = left; i < right; i++) {
+			if (Array[i + 1] > Array[i]) {
+				temp = Array[i];
+				Array[i] = Array[i + 1];
+				Array[i + 1] = temp;
+			}
+		}
+		right--;
+
+		for (int i = right; i > left; i--) {
+			if (Array[i - 1] < Array[i]) {
+				temp = Array[i];
+				Array[i] = Array[i - 1];
+				Array[i - 1] = temp;
+			}
+		}
+		left++;	
+	}
+
+	}
+
+int main()
+{
+
+	int Array[8]{ 44, 55, 12, 42, 94, 18, 6, 67 };
+
+	shaker(Array, 8);
+	std::cout << "Result: " << std::endl;
+	for (int i = 0; i < 8; i++) {
+		std::cout << Array[i] << " ";
+	}
+
+}
+
