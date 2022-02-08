a5c40c670e66ca969b77d1d3ec8c8674472dd5b5
Potapenko Egor <73658660+Shara1p@users.noreply.github.com>
2022-02-08 22:06:39 +0300

    Add files via upload

---
+++ Annagramma/Annagramma.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <algorithm>
+
+int main()
+{
+    std::string word;
+    std::cout << "Write down main word" << std::endl;
+    std::cin >>  word;
+    std::cout << "Write down amount of 'anagrams'" << std::endl;
+    int n = 0;
+    std::cin >> n;
+    std::cout << "Write down 'anagrams'" << std::endl;
+
+    std::string* Annagrams = new std::string[n];
+    int* AnnagramsHash = new int[n];
+    for (int i = 0; i < n; i++) {
+        std::cin >> Annagrams[i];
+        AnnagramsHash[i] = std::hash<std::string>{}(Annagrams[i]);
+    }
+
+
+    int fact = 1;
+    for (int i = 1; i <= word.length(); i++) {
+        fact *= i;
+    }
+
+    int* Wordhash = new int[fact];
+    
+    for (int i = 0; i < fact; i++) {
+        std::next_permutation(begin(word), end(word));
+        //std::cout << word << std::endl;
+        Wordhash[i] = std::hash<std::string>{}(word);
+    }
+    
+    for (int i = 0; i < fact; i++) {
+        for (int j = 0; j < n; j++) {
+            if (Wordhash[i] == AnnagramsHash[j]) {
+                std::cout << "Anagram: " << Annagrams[j];
+                exit(0);
+            }
+        }
+        
+    }
+    
+    
+}
