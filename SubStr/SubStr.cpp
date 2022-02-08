a45586a6de5fdf0044e7653e3a2a81890c926c6b
Potapenko Egor <73658660+Shara1p@users.noreply.github.com>
2022-02-08 22:36:18 +0300

    Add files via upload

---
+++ SubStr/SubStr.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+void sub(std::string string, int n) {
+    std::string Substr;
+    for (int i = 0; i < string.length(); i++) {
+        Substr = string.substr(i, n);
+        if (Substr.length() == n) {
+            std::cout << Substr << " ";
+        }
+    }
+}
+
+int main()
+{
+    std::string string;
+    std::cin >> string;
+    int n = 0;
+    std::cin >> n;
+    sub(string, n);
+}
---
+++ stdVector/stdVector.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    std::vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> a[i];
+    }
+    int k;
+    std::cin >> k;
+
+    std::vector<int> b;
+    for (int i = 0; i < n; ++i) {
+        if (a[i] < k) {
+            b.push_back(a[i]);
+        }
+    }
+
+    for (int i = 0; i < b.size(); ++i) {
+        std::cout << b[i] << " ";
+    }
+}
