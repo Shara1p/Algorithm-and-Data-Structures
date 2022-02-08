f2b1a493c56c7fcc861b903b3eb3b179ae4f62c8
Potapenko Egor <73658660+Shara1p@users.noreply.github.com>
2022-02-08 22:17:26 +0300

    Add files via upload

---
+++ PolishMath/PolishMath.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include "stack.h"
+#include <stdlib.h>
+
+
+void underflow(Stack* &stack1) {
+    stack_pop(stack1);
+    if (stack_empty(stack1)) {
+        std::cout << "UNDERFLOW";
+        exit(1);
+    }
+}
+
+void popPush(Stack* &stack1, int temptop) {
+    stack_pop(stack1);
+    stack_push(stack1, temptop);
+}
+
+int main()
+{
+
+    std::string polishMath;
+    std::string number;
+
+
+    Stack* stack1 = stack_create();
+    int temptop = 0;
+
+
+
+    std::cout << "Write down your math expression using after each operator " << "Enter. If something wasn't printed push your key again. " << std::endl;
+    std::cout << "After you've finished write down " << "! and push Enter and you will get the answer" << std::endl;
+
+    while (true) {
+
+        std::cin >> polishMath;
+        if (polishMath == "!") {
+            break;
+        }
+        switch (polishMath[0]) {
+        case '+':
+            underflow(stack1);
+            
+            temptop += stack_get(stack1);
+            popPush(stack1, temptop);
+            break;
+        case '-':
+            underflow(stack1);
+
+            temptop = stack_get(stack1) - temptop;
+            popPush(stack1, temptop);
+            break;
+        case '/':
+            underflow(stack1);
+
+            if (temptop == 0) {
+                std::cout << "ZERO";
+                exit(1);
+            }
+            temptop = stack_get(stack1) / temptop;
+            popPush(stack1, temptop);
+            break;
+        case '*':
+            underflow(stack1);
+
+            temptop *= stack_get(stack1);
+            popPush(stack1, temptop);
+            break;
+        default:
+            stack_push(stack1, std::atoi(polishMath.c_str()));
+            temptop = stack_get(stack1);
+            break;
+        }
+        
+    }
+
+    temptop = stack_get(stack1);
+    stack_pop(stack1);
+    if (stack_empty(stack1)) {
+        std::cout << "Answer: " << temptop << std::endl;
+    }
+    else {
+        std::cout << "OVERFLOW";
+        exit(1);
+    }
+
+
+    stack_delete(stack1);
+}
---
+++ PolishMath/stack.cpp
@@ -0,0 +1,50 @@
+#include "stack.h"
+#include "vector.h"
+
+struct Stack
+{
+    Vector* vector;
+    Stack() {
+        vector = vector_create();
+        vector_resize(vector, 0);
+    }
+
+};
+
+Stack* stack_create()
+{
+    return new Stack;
+}
+
+void stack_delete(Stack* stack)
+{
+    vector_delete(stack->vector);
+    delete stack;
+}
+
+void stack_push(Stack* stack, Data data)
+{
+    vector_resize(stack->vector, vector_size(stack->vector) + 1);
+    vector_set(stack->vector, vector_size(stack->vector) - 1, data);
+}
+
+Data stack_get(const Stack* stack)
+{
+    return vector_get(stack->vector, vector_size(stack->vector) - 1);
+}
+
+void stack_pop(Stack* stack)
+{
+    vector_resize(stack->vector, vector_size(stack->vector) - 1);
+}
+
+bool stack_empty(const Stack* stack)
+{
+    if (vector_size(stack->vector) > 0) {
+        return false;
+    }
+    else {
+        return true;
+    }
+
+}
